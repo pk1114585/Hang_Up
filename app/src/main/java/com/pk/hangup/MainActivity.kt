743bf13f12c5e15c1a4f2927d596a4ae12e2ece8
hang_up <pk1114585>
2021-09-09 23:26:41 +0530

    base

---
+++ app/src/main/java/com/pk/hangup/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pk.hangup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
