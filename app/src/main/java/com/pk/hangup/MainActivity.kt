c41883540a0824fd4611cfef62c319474bb0b8dd
Somesh <someshiet@gmail.com>
2021-09-10 00:48:56 +0530

    added click listener and data binding

--- app/src/main/java/com/pk/hangup/MainActivity.kt
+++ app/src/main/java/com/pk/hangup/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.pk.hangup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.pk.hangup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.idMainLoginBtn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this,Login::class.java)
+            startActivity(intent)
+        })
     }
 }
